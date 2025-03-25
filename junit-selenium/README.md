4931c91b9216df8dce3264fa1e96d719353c7226
Tri Dong Quang <tridq88@gmail.com>
2025-03-25 15:47:28 +0700

    Add Facebook login test

---
+++ junit-selenium/README.md
@@ -0,0 +1,4 @@
+==== Fix issue "Chrome mismatch version: This version of ChromeDriver only supports Chrome version 114"
+Current browser version is 134.0.6998.89
+Download the Chrome Webdriver for current version 134.x.x.x and put in as below:
+"C:\\Users\\tridq\\eclipse-workspace\\chromedriver-win64\\chromedriver.exe"
---
+++ junit-selenium/src/test/java/com/example/FacebookLogin.java
@@ -0,0 +1,85 @@
+package com.example;
+
+import java.util.regex.Pattern;
+import java.util.concurrent.TimeUnit;
+import org.junit.*;
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.apache.commons.io.FileUtils;
+import java.io.File;
+
+
+public class FacebookLogin {
+  private WebDriver driver;
+  private String baseUrl;
+  private boolean acceptNextAlert = true;
+  private StringBuffer verificationErrors = new StringBuffer();
+  JavascriptExecutor js;
+  @Before
+  public void setUp() throws Exception {
+    System.setProperty("webdriver.chrome.driver", "C:\\Users\\tridq\\eclipse-workspace\\chromedriver-win64\\chromedriver.exe");
+    driver = new ChromeDriver();
+    baseUrl = "https://www.google.com/";
+    driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+    js = (JavascriptExecutor) driver;
+  }
+
+  @Test
+  public void testFacebookLogin() throws Exception {
+    driver.get(baseUrl + "chrome://newtab/");
+    driver.get("https://www.facebook.com/");
+    driver.findElement(By.id("email")).click();
+    driver.findElement(By.id("email")).clear();
+    driver.findElement(By.id("email")).sendKeys("email@gmail.com");
+    driver.findElement(By.id("pass")).clear();
+    driver.findElement(By.id("pass")).sendKeys("password");
+    driver.findElement(By.id("pass")).click();
+    driver.findElement(By.id("pass")).sendKeys(Keys.ENTER);
+    Thread.sleep(5000);//Add sleep to see the login result
+  }
+
+  @After
+  public void tearDown() throws Exception {
+    driver.quit();
+    String verificationErrorString = verificationErrors.toString();
+    if (!"".equals(verificationErrorString)) {
+      fail(verificationErrorString);
+    }
+  }
+
+  private boolean isElementPresent(By by) {
+    try {
+      driver.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+
+  private boolean isAlertPresent() {
+    try {
+      driver.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
+
+  private String closeAlertAndGetItsText() {
+    try {
+      Alert alert = driver.switchTo().alert();
+      String alertText = alert.getText();
+      if (acceptNextAlert) {
+        alert.accept();
+      } else {
+        alert.dismiss();
+      }
+      return alertText;
+    } finally {
+      acceptNextAlert = true;
+    }
+  }
+}
